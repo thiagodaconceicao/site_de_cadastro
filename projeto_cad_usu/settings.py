0d63697e54af22c6d3427d4c878e9126226e23c2
Thiago da Conceição <thiagogoisconceicao@gmail.com>
2024-04-06 22:27:13 -0300

    fixing security mistake

--- projeto_cad_usu/settings.py
+++ projeto_cad_usu/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://*']
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
