3d0d8d5626ee61bee39ac4714edad9ad4892b666
Thiago da Conceição <thiagogoisconceicao@gmail.com>
2024-04-14 16:44:06 -0300

    add final unit test in selenium

--- project_files/tests.py
+++ project_files/tests.py
@@ -1,11 +1,22 @@
 from django.test import TestCase
 from django.test import LiveServerTestCase
-from selenium import webdriver
 
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
 class selenium_browser_test(LiveServerTestCase):
     def test_login(self):
         selenium = webdriver.Chrome()
-        selenium.get('http://localhost:8000')
-        insert_name = selenium.driver.findElement(By.id('nome'))
+        result = selenium.get('http://localhost:8000')
+        selenium.implicitly_wait(3)
+
+        print(result)
+
+        nome_element = selenium.find_element(By.ID, 'nome')
+        senha_element = selenium.find_element(By.ID, 'senha')
+
+        nome_element.send_keys('teste')
+        senha_element.send_keys('123456')
+         
