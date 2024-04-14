1d6b32dd26ec9c166defbe3be558c4c575fc14fd
Thiago da Conceição <thiagogoisconceicao@gmail.com>
2024-04-14 08:23:08 -0300

    add selenium tests

--- project_files/tests.py
+++ project_files/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
-import pytest_django
-# Create your tests here.
-class connection_test:
-    def online(self):
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
+
+# Create your tests here.
+class selenium_browser_test(LiveServerTestCase):
+    def test_login(self):
+        selenium = webdriver.Chrome()
+        selenium.get('http://localhost:8000')
+        insert_name = selenium.driver.findElement(By.id('nome'))
